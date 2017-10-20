58eb3737c2b82c8535994d0fd8a08c531c8eba57
simonkvannli <simon_kvannli@hotmail.com>
2017-10-20 11:15:41 +0200

    Add a little more details to oppgave7

--- oppgave7b.py
+++ oppgave7b.py
@@ -5,7 +5,7 @@ def check_equal(str1, str2):
         return False
     for i in range(len(str1)):
         if(str1[i]!=str2[i]):
-            return False
+            return True
     return True
 
 def reversed_word(str1):
@@ -31,19 +31,29 @@ class TestMethods(unittest.TestCase):
 	def test_1(self):
             str1 = 'hei'
             str2 = 'hello'
-            str3 = 'hello'
             stud_answer = check_equal(str1, str2)
+	    self.assertFalse(stud_answer)
+
+	def test_1b(self):
+            str2 = 'hello'
+            str3 = 'hello'
             stud_answer2 = check_equal(str3, str2)
-	    self.assertEqual(stud_answer, False)
-	    self.assertEqual(stud_answer2, True)
+	    self.assertTrue(stud_answer2)
+
+	def test_1c(self):
+            str2 = 'helko'
+            str3 = 'hello'
+            stud_answer2 = check_equal(str3, str2)
+	    self.assertFalse(stud_answer2, "check_equal funksjonen din tror 'helko' er lik 'hello'")
 
 	def test_2(self):
             str8 = 'Morna Jens'
             self.assertEqual(reversed_word(str8), "sneJ anroM")
 
+        def test_3(self):
             str1 = 'agnes i senga'
             str2 = 'hello'
-            self.assertEqual(check_palindrome(str1), True)
+            self.assertTrue(check_palindrome(str1))
 
         def test_4(self):
             str1 = 'pepperkake'
